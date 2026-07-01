3e5d02cc813c1e15b1cbf129ef46d8a27ff33a78
portosyncsite <marian2008.porto@gmail.com>
2026-07-01 14:04:17 +0300

    Add files via upload

---
+++ radio/galerie.php
@@ -0,0 +1,17 @@
+<?php include "header.php"; ?>
+
+<div class="container">
+
+<h1>Galerie</h1>
+
+<div class="gallery">
+
+<img src="img/moise.jpg">
+<img src="img/david.jpg">
+<img src="img/petru.jpg">
+
+</div>
+
+</div>
+
+<?php include "footer.php"; ?>
